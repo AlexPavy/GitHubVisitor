b0e41c3dfe5b0936fb526c5089fc030e69c8ff11
Alex Pavy <alex.g.pavy@gmail.com>
2016-10-29 23:04:18 +0200

    first commit

---
+++ src/main/java/alexpavy/applications/GithubvisitorApplication.java
@@ -0,0 +1,12 @@
+package alexpavy.applications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubvisitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubvisitorApplication.class, args);
+	}
+}
